ad2ea7c3825f16cf2f6c76dfa34afc452c04ac89
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-02-09 16:15:08 -0700

    Add files via upload

---
+++ Scripts/Awards.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Awards : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Score.OnMilestone += GiveAward;
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnDisable()
+    {
+        Score.OnMilestone -= GiveAward;
+    }
+
+    void GiveAward(float distance)
+    {
+        GetComponent<Text>().text = "Milestone: " + distance + " meters!";
+        animator.SetBool("reachMilestone", true);
+    }
+
+    //This function is only called at the end of an animation
+    public void ResetAnimation()
+    {
+        animator.SetBool("reachMilestone", false);
+    }
+}
---
+++ Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public delegate void Milestone(float distance);
+    public static event Milestone OnMilestone;
+
+    //Reference player position and text to update
+    public Transform player;
+    public Text scoreText;
+    public float milestoneIncrement = 150f;
+
+    //Update called once per frame
+    void Update()
+    {
+        //Update text with player position whole number
+        scoreText.text = player.position.z.ToString("0");
+
+        if (player.position.z >= milestoneIncrement)
+        {
+            OnMilestone(milestoneIncrement);
+            milestoneIncrement *= 3;
+        }
+    }
+}
